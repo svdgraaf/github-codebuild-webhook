d872c9d4227769a8f0601a1e38623d1b1497c4a0
Sander van de Graaf <mail@svdgraaf.nl>
2017-04-19 22:41:25 +0200

    use step functions to check for build progress

--- handler.js
+++ handler.js
@@ -1,33 +1,32 @@
 'use strict';
 
-var AWS = require('aws-sdk')
+var AWS = require('aws-sdk');
 var codebuild = new AWS.CodeBuild();
-var GitHubApi = require("github");
 
+var GitHubApi = require("github");
 var github = new GitHubApi();
+
+var AWSXRay = require('aws-xray-sdk');
+
+// setup github client
 github.authenticate({
     type: "basic",
     username: process.env.GITHUB_USERNAME,
-    password: process.env.GITHUB_KEY
+    password: process.env.GITHUB_ACCESS_TOKEN
 });
 
 // get the region where this lambda is running
 var region = process.env.AWS_DEFAULT_REGION;
 
-// time in minutes for which we'll keep updating the commit after the build is finished
-var diff_timelength = 60 * 5 * 1000;
-
 // this function will be triggered by the github webhook
-module.exports.trigger_build = (event, context, callback) => {
+module.exports.start_build = (event, context, callback) => {
 
-  // parse the github json event
-  var body = JSON.parse(event.body);
   var response;
 
-  // we only act on pull_request changes (can be any)
-  if('pull_request' in body) {
+  // we only act on pull_request changes (can be any, but we don't need those)
+  if('pull_request' in event) {
 
-    var head = body.pull_request.head;
+    var head = event.pull_request.head;
     var repo = head.repo;
 
     var params = {
@@ -39,11 +38,10 @@ module.exports.trigger_build = (event, context, callback) => {
     codebuild.startBuild(params, function(err, data) {
       if (err) {
         console.log(err, err.stack);
-        response = {
-          statusCode: 501,
-          body: JSON.stringify("Build start failed"),
-        };
+        callback(err);
       } else {
+
+        var build = data.build;
 
         // all is well, mark the commit as being 'in progress'
         github.repos.createStatus({
@@ -57,102 +55,65 @@ module.exports.trigger_build = (event, context, callback) => {
         }).then(function(data){
           console.log(data);
         });
-        response = {
-          statusCode: 201,
-          body: JSON.stringify("Build pending"),
-        };
-
+        callback(null, build);
       }
     });
   } else {
-    console.log('this was not a pull-request message');
-    response = {
-      statusCode: 412,
-      body: JSON.stringify("failed"),
-    };
+    callback("Not a PR");
   }
-  callback(null, response);
 }
 
-
 module.exports.check_build_status = (event, context, callback) => {
-  // get the build statuses for the given project name
   var params = {
-    projectName: process.env.BUILD_PROJECT
-  };
-
-  // list all builds, loop through. If it's a github build, update the commit
-  // for that build with the state of the codebuild status
-  codebuild.listBuildsForProject(params, function(err, data) {
+    ids: [event.id]
+  }
+  codebuild.batchGetBuilds(params, function(err, data) {
     if (err) {
       console.log(err, err.stack);
       context.fail(err)
+      callback(err);
     } else {
-      // list all the build statuses
-      codebuild.batchGetBuilds(data, function(err, data) {
-        if (err) {
-          console.log(err, err.stack);
-          context.fail(err)
-        } else {
-          for (let build of data.builds) {
-            if(build.source.type != 'GITHUB') {
-              continue;
-            }
-            // console.log(((new Date()) - new Date(build.endTime)), diff_timelength);
-
-            // because we run every X minutes, we only check the builds that we're finished in the last
-            // X minutes, so we don't call the github api unneeded
-            if(((new Date) - new Date(build.endTime)) > diff_timelength) {
-              console.log('Build [' + build.id + '] was updated more than X amount of time ago, skip it')
-              continue
-            }
-
-            // the build was complete, and updated in the last X minutes, let's update the git commit status
-            if(build.buildComplete) {
-
-              // get the necessary variables for the github call
-              var url = build.source.location.split('/');
-              var repo = url[url.length-1].replace('.git', '');
-              var username = url[url.length-2];
-
-              console.log('Found commit identifier: ' + build.sourceVersion);
-              var state = '';
-
-              // map the codebuild status to github state
-              switch(build.buildStatus) {
-                case 'SUCCEEDED':
-                  state = 'success';
-                  break;
-                case 'FAILED':
-                  state = 'failure';
-                  break;
-                case 'FAULT':
-                case 'STOPPED':
-                case 'TIMED_OUT':
-                  state = 'error'
-                default:
-                  state = 'pending'
-              }
-              console.log('Github state will be', state);
-
-              // call the github repo with the actual status
-              github.repos.createStatus({
-                owner: username,
-                repo: repo,
-                sha: build.sourceVersion,
-                state: state,
-                target_url: 'https://' + region + '.console.aws.amazon.com/codebuild/home?region=' + region + '#/builds/' + build.id + '/view/new',
-                context: 'CodeBuild',
-                description: 'Build ' + build.buildStatus + '...'
-              }).catch(function(err){
-                console.log(err);
-                context.fail(data);
-              });
-            }
-          }
-          context.succeed('Succeeded')
-        }
-      });
+      callback(null, data.builds[0]);
     }
+  });
+}
+
+module.exports.build_done = (event, context, callback) => {
+  // get the necessary variables for the github call
+  var url = event.source.location.split('/');
+  var repo = url[url.length-1].replace('.git', '');
+  var username = url[url.length-2];
+
+  console.log('Found commit identifier: ' + event.sourceVersion);
+  var state = '';
+
+  // map the codebuild status to github state
+  switch(event.buildStatus) {
+    case 'SUCCEEDED':
+      state = 'success';
+      break;
+    case 'FAILED':
+      state = 'failure';
+      break;
+    case 'FAULT':
+    case 'STOPPED':
+    case 'TIMED_OUT':
+      state = 'error'
+    default:
+      state = 'pending'
+  }
+  console.log('Github state will be', state);
+
+  github.repos.createStatus({
+    owner: username,
+    repo: repo,
+    sha: event.sourceVersion,
+    state: state,
+    target_url: 'https://' + region + '.console.aws.amazon.com/codebuild/home?region=' + region + '#/builds/' + event.id + '/view/new',
+    context: 'CodeBuild',
+    description: 'Build ' + event.buildStatus + '...'
+  }).catch(function(err){
+    console.log(err);
+    context.fail(data);
   });
 }
