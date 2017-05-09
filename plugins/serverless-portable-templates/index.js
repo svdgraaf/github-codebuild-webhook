de8d4170eb0951ff81a958a3de15338e23370c33
Sander van de Graaf <mail@svdgraaf.nl>
2017-05-09 12:11:25 +0200

    bump plugin

--- plugins/serverless-portable-templates/index.js
+++ plugins/serverless-portable-templates/index.js
@@ -8,30 +8,33 @@ class ServerlessPortableTemplates {
     this.serverless = serverless;
     this.options = options;
     this.hooks = {
-      'before:deploy:deploy': this.makePortableTemplate.bind(this),
+      'before:deploy:deploy': this.makePortableTemplate.bind(this)
     };
   }
 
   makePortableTemplate() {
     const template = this.serverless.service.provider.compiledCloudFormationTemplate;
+    var serviceName = this.serverless.service.service;
 
     // remove the RoleName, so CloudFormation will generate one for us
     delete template['Resources']['IamRoleLambdaExecution']['Properties']['RoleName'];
 
-    // remove the name for loggroups, so CF will generate a name for us (actually)
-    // those resources are not needed at all, but it's more work to remove them
     _.each(template['Resources'], function(resource, name){
+
+      // remove the name for loggroups, so CF will generate a name for us (actually)
+      // those resources are not needed at all, but it's more work to remove them
       if(resource['Type'] == 'AWS::Logs::LogGroup') {
         delete resource['Properties']['LogGroupName']
         template['Resources'][name] = resource
       }
 
+      // replace the service name with the stack name so the functions will be unique
+      // per account
       if(resource['Type'] == 'AWS::Lambda::Function') {
-        resource['Properties']['FunctionName'] = {
-          "Fn::Sub": resource['Properties']['FunctionName'] + "-${AWS::StackName}"
-        }
+        resource['Properties']['FunctionName'] = resource['Properties']['FunctionName'].replace(serviceName, "#{AWS::StackName}")
         template['Resources'][name] = resource
       }
+
     })
 
     this.serverless.cli.consoleLog('Made template portable');
